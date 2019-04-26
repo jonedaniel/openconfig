3718f20b6b9e1584424c1ed06d86e5d93fd14ca6
zmh_new <zhaomenghui93@163.com>
2019-04-27 01:38:36 +0800

    jenkins deploy jar

--- config_file/run-jar-withlog.sh
+++ config_file/run-jar-withlog.sh
@@ -1,4 +1,19 @@
-logname=$(date "+%Y-%m-%d-%H-%M-%S").log
+tell(){
+	if [ $? -eq 0 ]
+    then
+        echo "执行成功"
+    else
+        echo "执行失败"
+    fi
+}
+echo "开始杀进程,端口:"$1
+kill -9 $(lsof -i:$1|tail -1|awk '"$1"!=""{print $2}')
+tell
+
+logname=log/$(date "+%Y-%m-%d-%H-%M-%S").log
+mkdir log
 touch $logname
-jarname=$1
-nohup java -jar $jarname >$logname
+jarname=$2
+echo "开始部署jar包"
+nohup java -jar $jarname > $logname &
+tell
